1759b614a405093bf50aaa87eeb852f35b02bc66
Gal A. Kaminka <galk@cs.biu.ac.il>
2025-03-06 22:32:41 +0200

    Created cmd.sh to run vhpop with specific default parameters in experiments

---
+++ cmd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+h='-h ADDR/ADDR_WORK/BUC/LIFO'
+f='-f {n,s}LR/{l}MW_add -l 12000 -f {n,s}LR/{u}MW_add -l 100000 -f {n,s,l}LR -l 240000 -f {n,s,u}LR/{l}LR -l unlimited'
+
+landmark=$1
+domain=$2
+problem=$3
+
+vhpop $h $f $landmark $domain $problem
