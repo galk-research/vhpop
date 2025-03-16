66bbc5ce71fc92a5125ae1072ce52d9a426431df
Gal A. Kaminka <galk@cs.biu.ac.il>
2025-03-16 11:45:35 +0200

    cmd.sh uses level 4 of tracing

--- cmd.sh
+++ cmd.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 h='-h ADDR/ADDR_WORK/BUC/LIFO'
+h=""
 f='-f {n,s}LR/{l}MW_add -l 12000 -f {n,s}LR/{u}MW_add/{l}MW_add -l 100000 -f {n,s,l}LR -l 240000 -f {n,s,u}LR/{l}LR -l unlimited'
+f=""
 
 landmark=$1
 domain=$2
 problem=$3
 
-./vhpop -g -v1 -m $landmark $h $f $domain $problem
+./vhpop -g -v4 -m $landmark $h $f $domain $problem
