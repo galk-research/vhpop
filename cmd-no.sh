da0fabe055f0aba4b1b53bfcb7e6a0b016fd1093
Gal A. Kaminka <galk@cs.biu.ac.il>
2025-11-29 16:53:59 +0200

    fixed gitignore. removed generated files.

---
+++ cmd-no.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+h='-h ADDR/ADDR_WORK/BUC/LIFO'
+h=""
+f='-f {n,s}LR/{l}MW_add -l 12000 -f {n,s}LR/{u}MW_add/{l}MW_add -l 100000 -f {n,s,l}LR -l 240000 -f {n,s,u}LR/{l}LR -l unlimited'
+f=""
+
+landmark=$1
+domain=$2
+problem=$3
+
+./vhpop -g -v4 $h $f $domain $problem
